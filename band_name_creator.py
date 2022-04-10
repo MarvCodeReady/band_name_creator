8a07b5065926fc23b267a6fb8ed77ff05ea32b2a
Marvin M <marvcodeready@gmail.com>
2022-04-09 21:41:24 -0700

    creation of band name generator with comments

---
+++ band_name_creator.py
@@ -0,0 +1,7 @@
+print("Welcolme to the Band Name Generator.")
+#asks the first question
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pets name?\n")
+#store user input in variables
+print("Your Brand name should be " + city_name + pet_name + ".")
+#prints final result in the end
